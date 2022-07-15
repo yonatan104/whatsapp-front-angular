89774b7852669fda04b37c694424907214587039
yonatan104 <yonatangeller@gmail.com>
2022-07-15 14:58:36 +0300

    logout is done without good ui later to improve ui to add menu cmp

--- src/app/cmps/contact/contact-app-header/contact-app-header.component.ts
+++ src/app/cmps/contact/contact-app-header/contact-app-header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ContactService } from 'src/app/services/contact.service';
+import { Router } from '@angular/router'
 @Component({
   selector: 'contact-app-header',
   templateUrl: './contact-app-header.component.html',
   styleUrls: ['./contact-app-header.component.scss']
 })
 export class ContactAppHeaderComponent implements OnInit {
+  
 
-  constructor() { }
-
+  constructor(private contactService: ContactService, private router: Router) { }
+  imgUrl: string = this.contactService.getLoggedinUser().imgUrl
   ngOnInit(): void {
+  }
+
+  onLogout(){
+    console.log('onLogout');
+    
+    this.contactService.logout()
+    this.router.navigateByUrl('')
   }
 
 }
