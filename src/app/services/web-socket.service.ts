a4eeb720c888c44061c2feccc308c26f84715d75
yonatan104 <yonatangeller@gmail.com>
2022-07-24 11:49:44 +0300

    set up code for first deployment fixing video ui fixing websockets for running in public mode

--- src/app/services/web-socket.service.ts
+++ src/app/services/web-socket.service.ts
@@ -1,25 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-// import * as io from 'socket.io-client'
 import { io } from "socket.io-client";
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class WebSocketService {
+  readonly uri: string = environment.production ? '' : 'ws://localhost:3030'
   socket: any
-  readonly uri: string = 'ws://localhost:3030'
   constructor() {
     this.socket = io(this.uri)
   }
-  listen(eventName:string){
-    return new Observable((subscriber)=>{
-      this.socket.on(eventName, (data:any)=>{
+  listen(eventName: string) {
+    return new Observable((subscriber) => {
+      this.socket.on(eventName, (data: any) => {
         subscriber.next(data)
       })
     })
   }
-  emit(eventName:string, data: any){
+  emit(eventName: string, data: any) {
     this.socket.emit(eventName, data)
   }
 }
